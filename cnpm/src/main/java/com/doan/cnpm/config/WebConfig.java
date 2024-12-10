530b9f6514924ca7984aff366068fec5cf02a068
quangthangk4 <quangthangkhmt.k22@gmail.com>
2024-12-10 22:56:23 +0700

    add CORS

---
+++ cnpm/src/main/java/com/doan/cnpm/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.doan.cnpm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Cho phép tất cả các request từ localhost:3000
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")  // Cấp phép localhost:3000
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Các phương thức HTTP cho phép
+                .allowedHeaders("*")  // Các header cho phép
+                .allowCredentials(true);  // Cho phép cookies hoặc thông tin xác thực
+    }
+}
